8fb373a066c750f77cbb5c100210bec5c798f7fe
John Leen <jleen@saturnvalley.org>
2016-12-19 18:00:44 -0500

    Adjustable max purchase

--- rebalance.py
+++ rebalance.py
@@ -3,11 +3,12 @@ from tabulate import tabulate
 import fileinput
 import re
 
+TOTAL_PURCHASE = 99999
 desired_ratio = {
-        'VBTLX': 7.5,
-        'VTABX': 2.5,
-        'VTIAX': 22.5,
-        'VTSAX': 67.5
+        'VBTLX': 7.5 / 100,
+        'VTABX': 2.5 / 100,
+        'VTIAX': 22.5 / 100,
+        'VTSAX': 67.5 / 100
         }
 funds = desired_ratio.keys()
 
@@ -32,23 +33,37 @@ for line in list(fileinput.input()):
         match = PARSE2.match(line)
         if not match:
             raise Exception('no cashooooola')
-        current_holdings[fund] = int(
-                match.group(1).replace(',', '').replace('.', ''))
+        current_holdings[fund] = float(match.group(1).replace(',', ''))
         fund = None
 
 total_holdings = sum(current_holdings.values())
-current_ratio = {k: v / total_holdings for [k, v] in current_ratio.items()}
+current_ratio = {k: v / total_holdings for [k, v] in current_holdings.items()}
 weight = {k: current_holdings[k] / desired_ratio[k] for k in funds}
 max_weight = max(weight.values())
 minimum_purchase = {k: max_weight * desired_ratio[k] - current_holdings[k]
                     for k in funds}
-new_holdings = {k: minimum_purchase[k] + current_holdings[k] for k in funds}
+min_total_purchase = sum(minimum_purchase.values())
+extra_total_purchase = TOTAL_PURCHASE - min_total_purchase
+
+assert extra_total_purchase >= 0
+extra_purchase = {k: extra_total_purchase * desired_ratio[k] for k in funds}
+
+purchase = {k: minimum_purchase[k] + extra_purchase[k] for k in funds}
+
+new_holdings = {k: purchase[k] + current_holdings[k] for k in funds}
+total_new_holdings = sum(new_holdings.values())
+new_ratio = {k: v / total_new_holdings for [k, v] in new_holdings.items()}
 
 def monetize(amt):
-    return '${:,.2f}'.format(amt / 100)
+    return '${: >10,.2f}'.format(amt)
+
+def percentize(rat):
+    return '{:>8.2f}%'.format(rat * 100)
 
 print(tabulate(
         [[k, monetize(current_holdings[k]),
-          monetize(minimum_purchase[k]), monetize(new_holdings[k])]
+          monetize(purchase[k]), monetize(new_holdings[k]),
+          percentize(current_ratio[k]), percentize(new_ratio[k])]
          for k in funds],
-        headers=['fund', 'current', 'buy', 'new']))
+        headers=['fund', 'current', 'buy', 'new', 'crnt%', 'new%'],
+        stralign='right'))
