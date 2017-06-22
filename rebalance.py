d101ed40a268e79c05be6614b6a66392fd8e542e
John Leen <jleen@saturnvalley.org>
2017-06-21 20:38:36 -0700

    Better algorithm for partial rebalance

--- rebalance.py
+++ rebalance.py
@@ -1,15 +1,16 @@
+from collections import OrderedDict
 from tabulate import tabulate
 
 import fileinput
 import re
 
 TOTAL_PURCHASE = 9999
-desired_ratio = {
-        'VBTLX': 7.5 / 100,
-        'VTABX': 2.5 / 100,
-        'VTIAX': 22.5 / 100,
-        'VTSAX': 67.5 / 100
-        }
+desired_ratio = OrderedDict([
+        ('VBTLX', 7.5 / 100),
+        ('VTABX', 2.5 / 100),
+        ('VTIAX', 22.5 / 100),
+        ('VTSAX', 67.5 / 100)
+        ])
 
 
 def monetize(amt):
@@ -28,8 +29,6 @@ PARSE2 = re.compile(r'^\$([0-9,]*\.[0-9][0-9])$')
 fund = None
 current_holdings = {}
 current_ratio = {}
-weight = {}
-minimum_purchase = {}
 new_holdings = {}
 
 for line in list(fileinput.input()):
@@ -47,25 +46,47 @@ for line in list(fileinput.input()):
 
 total_holdings = sum(current_holdings.values())
 current_ratio = {k: v / total_holdings for [k, v] in current_holdings.items()}
-weight = {k: current_holdings[k] / desired_ratio[k] for k in funds}
-max_weight = max(weight.values())
-minimum_purchase = {k: max_weight * desired_ratio[k] - current_holdings[k]
-                    for k in funds}
-min_total_purchase = sum(minimum_purchase.values())
-extra_total_purchase = TOTAL_PURCHASE - min_total_purchase
 
-if extra_total_purchase >= 0:
+# Iterate through the funds in order from most underbalanced to most
+# overbalanced. For each one, top off the funds that are *more underbalanced*
+# until they balance with the current fund. Continue in this manner until all
+# funds are balanced with the most overbalanced fund, or until we run out of
+# money.
+weights = {k: current_holdings[k] / desired_ratio[k] for k in funds}
+purchase = {k: 0 for k in funds}
+remaining_purchase = TOTAL_PURCHASE
+recipient_funds = []
+
+for (fund, weight) in sorted(weights.items(), key=lambda t: t[1]):
+    if len(recipient_funds) > 0:
+        tentative_holdings = {k: current_holdings[k] + purchase[k]
+                              for k in funds}
+        min_purchase = {k: weight * desired_ratio[k] - tentative_holdings[k]
+                        for k in recipient_funds}
+        min_total_purchase = sum(min_purchase.values())
+
+        if remaining_purchase > min_total_purchase:
+            # So far we've managed to do a partial rebalance without running
+            # out of money, so tally up what we're buying so far, and continue.
+            for [k, v] in min_purchase.items():
+                purchase[k] += v
+            remaining_purchase -= min_total_purchase
+        else:
+            # We haven't got enough money left to finish this (or any future)
+            # stage of the rebalance, so allocate the remaining money
+            # proportionally, and we're done.
+            for [k, v] in min_purchase.items():
+                purchase[k] += v * (remaining_purchase / min_total_purchase)
+            remaining_purchase = 0
+            break
+
+    recipient_funds += [fund]
+
+if remaining_purchase > 0:
     # We're purchasing more than enough to reach the target ratio,
     # so distribute the rest proportionally to maintain the target ratio.
-    extra_purchase = {k: extra_total_purchase * desired_ratio[k]
-                      for k in funds}
-    purchase = {k: minimum_purchase[k] + extra_purchase[k] for k in funds}
-else:
-    # We're not going to reach the target, so allocate funds in proportion to
-    # the purchases that *would* reach the target.
-    purchase = {
-            k: minimum_purchase[k] * TOTAL_PURCHASE / min_total_purchase
-            for k in funds}
+    for k in funds:
+        purchase[k] += remaining_purchase * desired_ratio[k]
 
 new_holdings = {k: purchase[k] + current_holdings[k] for k in funds}
 total_new_holdings = sum(new_holdings.values())
